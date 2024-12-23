4627b489e837eb1a044916eba0b05e94d825a625
fufu-2345 <mr.kittisak.ploysumrit@gmail.com>
2024-12-23 18:18:40 +0700

    2.1 done

--- hw 1/2.1.cpp
+++ hw 1/2.1.cpp
@@ -1,29 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	bool check=true;
-	int n,find;
-	cin>>n>>find;
-	
-	int arr[n];
-	for(int i=0;i<n;i++){
-		cin>>arr[i];	
-	}
-	
-	int low=0;
-	int high=n-1;
-	while(find>=arr[low] && find<=arr[high]){
-		int posi=low+   (   (   (find-arr[low])*(high-low)   ) /  (arr[high] - arr[low])    );
-		cout<<posi<<" ";
-		if(arr[posi]==find){
-			break;
-		}
-		if(posi>=low){
-			low=posi+1;
-		}
-		else{
-			high=posi-1;
-		}
-	}
+void inperola(int arr[], int low, int high, int find)
+{
+    int posi = low + ((find - arr[low]) * (high - low) / (arr[high] - arr[low]));
+    cout << posi << " ";
+
+    if (arr[posi] == find)
+    {
+        return;
+    }
+    else if (posi >= low)
+    {
+        inperola(arr, posi + 1, high, find);
+    }
+    else
+    {
+        inperola(arr, low, posi - 1, find);
+    }
+}
+
+int main()
+{
+    int n, find;
+    cin >> n >> find;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int low = 0;
+    int high = n - 1;
+    inperola(arr, low, high, find);
 }
