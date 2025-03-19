06a5ecccdf5e4d13eea6a67a2e35c34de7866fa4
fufu-2345 <mr.kittisak.ploysumrit@gmail.com>
2025-03-19 23:26:33 +0700

    fix 7.1.4

--- hw 7/7.1/7.1.4.cpp
+++ hw 7/7.1/7.1.4.cpp
@@ -4,44 +4,42 @@ using namespace std;
 
 int n;
 int a,b;
-vector<vector<int>> arr;
 
-void swap(int *a,int *b){
-	int temp=*a;
-	*a=*b;
-	*b=temp;
-}
+class present{
+	public:
+	int more;
+	int less;
+	int diff;
+	
+	present(int m,int l,int d):more(m),less(l),diff(d){}
+
+};
+
+vector<present> presents;
 
 void sort(){
 	for(int k=n/2;k>0;k/=2){
 		for(int i=0;i<n;i++){
-			for(int j=i;j>=k && arr[j][2]>arr[j-k][2];j-=k){
-				int temp=arr[j][0];
-				int temp1=arr[j][1];
-				int temp2=arr[j][2];
-				
-				arr[j][0]=arr[j-k][0];
-				arr[j][1]=arr[j-k][1];
-				arr[j][2]=arr[j-k][2];
-				
-				arr[j-k][0]=temp;
-				arr[j-k][1]=temp1;
-				arr[j-k][2]=temp2;
+			for(int j=i;j>=k &&     presents[j].diff>presents[j-k].diff       ;j-=k){
+				present temp=presents[j];
+                presents[j]=presents[j-k];
+                presents[j-k]=temp;
 			}
 		}
 	}
 }
 
-void present(){
+void cal(){
 	for(int i=0;i<n;i++){
 		if(a>=b){
-			a+=arr[i][1];
-			b+=arr[i][0];
+			a+=presents[i].less;
+			b+=presents[i].more;
 		}
 		else{
-			a+=arr[i][0];
-			b+=arr[i][1];
+			a+=presents[i].more;
+			b+=presents[i].less;
 		}
+		cout<<a<<" "<<b<<endl;
 	}
 }
 
@@ -50,15 +48,18 @@ int main(){
 	for(int i=0;i<n;i++){
 		cin>>a>>b;
 		if(a<b){
-			swap(&a,&b);
+			int temp=a;
+			a=b;
+			b=temp;
 		}
-		arr.push_back({a,b,a-b});
+		presents.push_back(present(a,b,a-b));
 	}
 	
 	sort();
 
 	a=b=0;
-	present();
+	cout<<endl<<endl;
+	cal();
 	
 	if(a>b){
 		cout<<endl<<endl<<a-b;
